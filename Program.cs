4106a85e17aee0e117d1f395d5038362d5ebd64d
Lindemberg da Silva Justino <lindembergparaiba@gmail.com>
2025-08-29 15:43:01 -0300

    feat: desafio poo concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nk = new Nokia("numero", "modelo", "imeiifbnjnbn", 32);
+
+nk.InstalarAplicativo("Fazebook");
+
+Iphone i = new Iphone("Numero", "modelo", "imei1546465484", 64);
+
+nk.InstalarAplicativo("Maçã");
